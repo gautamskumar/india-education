53b98021303041b25cedc9903e2d5be7e2bc2cbb
gautamskumar <gkumar09@gmail.com>
2018-06-21 09:19:17 -0400

    Starting project

---
+++ india-education/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, json, request, session, url_for, escape, redirect, jsonify, make_response
+from werkzeug import generate_password_hash, check_password_hash
+from flask.ext.pymongo import PyMongo
+import pymongo
+from pymongo import MongoClient
+from bson.son import SON
+
+app     = Flask(__name__)
+mongo   = PyMongo(app)
+
+@app.route('/')
