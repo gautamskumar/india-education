ec1cdc765d4d90df5528f40613e0a88f8defed9d
gautamskumar <gkumar09@gmail.com>
2018-06-21 11:20:47 -0400

    Operational locally

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app     = Flask(__name__)
 mongo   = PyMongo(app)
 
 @app.route('/')
+def homePage():
 	return render_template('index.html')
 
-@app.route('/hi')
-	return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
