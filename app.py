1f2e129271590c2169591fc84212b9a67eda74f6
gautamskumar <gkumar09@gmail.com>
2018-06-21 15:14:15 -0400

    form

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask.ext.pymongo import PyMongo
 import pymongo
 from pymongo import MongoClient
 from bson.son import SON
+import pandas as pd
 
 app     = Flask(__name__)
 mongo   = PyMongo(app)
@@ -14,6 +15,32 @@ mongo   = PyMongo(app)
 @app.route('/')
 def homePage():
 	return render_template('index.html')
+
+@app.route('/<classID>')
+def classForm(classID):
+	cID = int(classID)
+	classrooms = [
+					{
+						"id"		: 1,
+						"students"	: [
+										{ "firstName": "Anshu", "lastName": "Jain" },
+										{ "firstName": "Raghav", "lastName": "Jain" },
+										{ "firstName": "Shalini", "lastName": "Jain" }
+									]
+					},
+					{
+						"id"		: 2,
+						"students"	: [
+										{ "firstName": "Preeti", "lastName": "Gupta" },
+										{ "firstName": "Tanya", "lastName": "Gupta" },
+										{ "firstName": "Shantanu", "lastName": "Gupta" }
+									]
+					}
+				]
+	cr = pd.DataFrame(classrooms)
+	cr = cr.loc[cr.id == cID]
+	students = pd.DataFrame(list(cr.students)[0])
+	return render_template('classroom.html', students = students)
 
 if __name__ == '__main__':
     app.run(debug=True)
